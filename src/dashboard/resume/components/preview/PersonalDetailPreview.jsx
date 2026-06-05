8e2c624eed189a98d9b49728b3653b863981182e
Swechhya <swechhhya.stha@email.com>
2026-06-05 17:11:42 +0545

    refix

--- src/dashboard/resume/components/preview/PersonalDetailPreview.jsx
+++ src/dashboard/resume/components/preview/PersonalDetailPreview.jsx
@@ -1,41 +1,37 @@
-
-
-function ExperiencePreview({ resumeInfo }) {
+function PersonalDetailPreview({ resumeInfo }) {
   const theme = resumeInfo?.themecolor || resumeInfo?.themeColor || '#2a6ef5'
 
   return (
-    <div className='my-6'>
-      <h2 className='text-center font-bold text-sm mb-2' style={{ color: theme }}>
-        Professional Experience
+    <div>
+      <h2
+        className='font-bold text-xl text-center'
+        style={{ color: theme }}
+      >
+        {resumeInfo?.firstName} {resumeInfo?.lastName}
       </h2>
-      <hr style={{ borderColor: theme }} />
 
-      {resumeInfo?.Experience?.map((experience, index) => (
-        <div key={index} className='my-5'>
-          <h2 className='text-sm font-bold' style={{ color: theme }}>
-            {experience?.title}
-          </h2>
-          <h2 className='text-xs flex justify-between'>
-            <span>
-              {[experience?.companyName, experience?.city, experience?.state]
-                .filter(Boolean)
-                .join(', ')}
-            </span>
-            <span>
-              {experience?.startDate}
-              {(experience?.endDate || experience?.currentlyWorking)
-                ? ` To ${experience?.currentlyWorking ? 'Present' : experience?.endDate}`
-                : ''}
-            </span>
-          </h2>
-          <div
-            className='text-xs my-2 [&>ul]:list-disc [&>ul]:ml-4 [&>ul]:pl-2 [&>p]:text-justify [&>ul>li]:text-justify'
-            dangerouslySetInnerHTML={{ __html: experience?.workSummery }}
-          />
-        </div>
-      ))}
+      <h2 className='text-center text-sm font-medium my-1'>
+        {resumeInfo?.jobTitle}
+      </h2>
+
+      <h2
+        className='text-center font-normal text-xs'
+        style={{ color: theme }}
+      >
+        {resumeInfo?.address}
+      </h2>
+
+      <div
+        className='flex justify-between text-xs font-normal mt-1'
+        style={{ color: theme }}
+      >
+        <h2>{resumeInfo?.Phone}</h2>
+        <h2>{resumeInfo?.email}</h2>
+      </div>
+
+      <hr className='my-2' style={{ borderColor: theme }} />
     </div>
   )
 }
 
-export default ExperiencePreview
+export default PersonalDetailPreview
